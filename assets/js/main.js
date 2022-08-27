a50604fa9f4c10d0d10bc997cf4068b28cefe246
Beatriz Azevedo <beatriznazevedo@gmail.com>
2022-08-27 16:02:16 -0300

    firebase

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,16 @@
+const txtName = document.getElementById('txtName')
+const txtEmail = document.getElementById('txtEmail')
+const txtLevel = document.getElementById('txtLevel')
+const txtCharacter = document.getElementById('txtCharacter')
 
-import { subscribeToHellfireClube } from './data/hellfire-clube.js'
+const btnSubscribe = document.getElementById('btnSubscribe')
 
-(function main() {
-    const txtName = document.getElementById('txtName')
-    const txtEmail = document.getElementById('txtEmail')
-    const txtLevel = document.getElementById('txtLevel')
-    const txtCharacter = document.getElementById('txtCharacter')
-
-    document.getElementById('btnSubscribe').addEventListener('click', async () => {
-        const subscribe = {
-            name: txtName.value,
-            email: txtEmail.value,
-            level: txtLevel.value,
-            character: txtCharacter.value
-        }
-
-        const id = await subscribeToHellfireClube(subscribe)
-        alert(`Inscrição ${id} adicionada com sucesso!`)
-    })
-})()
+btnSubscribe.addEventListener('click', () => {
+    const subscription = {
+        name: txtName.value, 
+        email: txtEmail.value,
+        level: txtLevel.value, 
+        character: txtCharacter.value,
+    }
+    console.log(subscription)
+})
