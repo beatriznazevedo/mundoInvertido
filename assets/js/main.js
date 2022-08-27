f72252770b0af34369eae51747e0b034c0c74cb8
Beatriz Azevedo <beatriznazevedo@gmail.com>
2022-08-27 19:10:21 -0300

    favicon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,29 @@
-(function main() {
-    const txtName = document.getElementById('txtName')
-    const txtEmail = document.getElementById('txtEmail')
-    const txtLevel = document.getElementById('txtLevel')
-    const txtCharacter = document.getElementById('txtCharacter')
+window.addEventListener('click', function() {
+    const audio = document.getElementById('music');
+    audio.play();
+    audio.volume = 0.2;
+  });
 
-    document.getElementById('btnSubscribe').addEventListener('click', async () => {
-        const subscribe = {
-            name: txtName.value,
-            email: txtEmail.value,
-            level: txtLevel.value,
-            character: txtCharacter.value
-        }
+  function switchTheme() {
+    document.body.classList.toggle('dark-theme');
+    document.body.classList.toggle('light-theme');
 
-        const id = await subscribeToHellfireClube(subscribe)
-        alert(`Inscrição ${id} adicionada com sucesso!`)
-    })
-})
+    document.body.setAttribute('aria-label', 'Trocando o tema');
+
+    const theme = document.body.classList[0];
+    const music = theme === 'light-theme' ? 'normal-world.mpeg' : 'inverted-world.mpeg'
+
+    const audio = document.getElementById('music');
+    audio.src = `assets/musics/${music}`;
+    audio.play();
+    audio.volume = 0.2;
+  }
+
+const btnSubscribe = document.getElementById('btnSubscribe')
+
+btnSubscribe.addEventListener('click', btnClick)
+
+  function btnClick () {
+
+        alert('Inscrição realizada com sucesso')
+}
